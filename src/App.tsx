047caf6160af306e0544ce6bfbb1029ac35be360
King <kingsley.adarkwah@apprenticefs.com>
2023-12-12 22:50:47 -0500

    add lottie animation to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <h1 className='text-3xl font-bold '>JOB SEARCH</h1>
+      <Home />
     </div>
   )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Lottie from "lottie-react";
+import hiringAnimation from '../../assets/lotties/hiring-lottie.json'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className='w-screen h-screen flex-col justify-center'>
+      <h1 className='text-lg'>Looking for a new career?</h1>
+      <Lottie 
+        animationData={hiringAnimation} 
+        loop={true} 
+        style={{ height: '80%', width: '50%'}}
+        />
+    </main>
   )
 }
 
