c97af7e0c71f3c7bd3f0be0f771e4531129fe575
King <kingsley.adarkwah@apprenticefs.com>
2023-12-14 01:42:48 -0500

    continue working on redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import dotenv from 'dotenv'
-
+import { Provider } from 'react-redux'
+import {store} from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
