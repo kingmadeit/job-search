986253df0855af5ea2920e90c3777acf8a149a24
King <kingsley.adarkwah@apprenticefs.com>
2023-12-19 01:13:00 -0500

    add banner image for detail component

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,6 +1,7 @@
 import hiringAnimation from './lotties/hiring-lottie.json'
 import searchAnimation from './lotties/searching-lottie.json'
 import noResults from './lotties/no-results-lottie.json'
+import detailBanner from './detailBanner.jpg'
 export {
-    hiringAnimation, searchAnimation, noResults
+    hiringAnimation, searchAnimation, noResults, detailBanner
 }
